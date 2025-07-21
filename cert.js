0cd19d570f51d9a57e7d30055f6a0a4463aa7ff7
Nelsweb <nelsboy001@gmail.com>
2025-07-21 09:28:49 +0100

    Delete cert.js

--- cert.js
+++
@@ -1,7 +0,0 @@
-// Example certificate generation
-function generateCertificate(name) {
-  const doc = new jsPDF();
-  doc.text("Certificate of Completion", 20, 30);
-  doc.text("Awarded to: " + name, 20, 50);
-  doc.save("certificate.pdf");
-}
